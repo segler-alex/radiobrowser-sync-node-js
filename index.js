77807354b5d018eaf0c132a3430638e7f4b2d92e
Unknown <segler_alex@web.de>
2018-01-28 23:22:11 +0100

    do incremental sync

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const rp = require('request-promise');
 const async = require('async');
 
 const SYNC_BASE_URL = process.env.SYNC_BASE_URL || exitWithError('SYNC_BASE_URL');
+const USER_AGENT = 'radiobrowsersync/0.0.1';
+const SYNC_INTERVAL = parseInt(process.env.SYNC_INTERVAL) || exitWithError('SYNC_INTERVAL');
+const SYNC_INTERVAL_MSEC = SYNC_INTERVAL * 1000;
 
 function retryPromise(times, interval, cb_p){
   return new Promise((resolve,reject)=>{
@@ -30,59 +33,113 @@ function exitWithError(str) {
   process.exit(1);
 }
 
-retryPromise(10, 3000, function () {
-  console.log('Connecting to db..');
-  return db.sequelize.authenticate();
-})
-  .then(() => {
-    console.log('OK');
-    console.log('Recreate db tables..');
-    return db.sequelize.sync({
-      force: true
-    });
-  })
-  .then(() => {
-    console.log('OK');
+function initialSync(){
+  return Promise.resolve().then(() => {
     console.log('Get stations from webservice..');
-    var options = {
+    let options = {
       uri: SYNC_BASE_URL + '/json/stations',
       headers: {
-        'User-Agent': 'radiobrowsersync/0.0.1'
+        'User-Agent': USER_AGENT
       },
       json: true
     };
     return rp(options);
   })
   .then((stations)=>{
-    console.log('OK');
-    var all = [];
+    let all = [];
     // console.log(JSON.stringify(stations[0],null,' '));
     console.log('Importing ' + stations.length + ' stations..');
-    for (var i=0;i<stations.length;i++){
+    for (let i=0;i<stations.length;i++){
+      let obj = stations[i];
       all.push({
-        Name: stations[i].name,
-        Url: stations[i].url,
-        Homepage: stations[i].homepage,
-        Favicon: stations[i].favicon,
-        Uuid: stations[i].stationuuid,
-        Tags: stations[i].tags,
-        Country: stations[i].country,
-        Subcountry: stations[i].state,
-        Language: stations[i].language,
-        Votes: stations[i].votes,
-        NegativeVotes: stations[i].negativevotes,
-        Creation: stations[i].lastchangetime
+        Name: obj.name,
+        Url: obj.url,
+        Homepage: obj.homepage,
+        Favicon: obj.favicon,
+        Uuid: obj.stationuuid,
+        Tags: obj.tags,
+        Country: obj.country,
+        Subcountry: obj.state,
+        Language: obj.language,
+        Votes: obj.votes,
+        NegativeVotes: obj.negativevotes,
+        Creation: obj.lastchangetime
       });
     }
     return db.Station.bulkCreate(all);
+  });
+}
+
+function incrementalSync(seconds){
+  return Promise.resolve().then(() => {
+    console.log('Get incremental changes from webservice..');
+    let options = {
+      uri: SYNC_BASE_URL + '/json/stations/changed?seconds=' + seconds,
+      headers: {
+        'User-Agent': USER_AGENT
+      },
+      json: true
+    };
+    return rp(options);
+  })
+  .then((stations)=>{
+    let all = [];
+    // console.log(JSON.stringify(stations[0],null,' '));
+    console.log('Importing ' + stations.length + ' changes..');
+    for (let i=0;i<stations.length;i++){
+      let obj = stations[i];
+      all.push({
+        Name: obj.name,
+        Url: obj.url,
+        Homepage: obj.homepage,
+        Favicon: obj.favicon,
+        Uuid: obj.stationuuid,
+        ChangeUuid: obj.changeuuid,
+        Tags: obj.tags,
+        Country: obj.country,
+        Subcountry: obj.state,
+        Language: obj.language,
+        Votes: obj.votes,
+        NegativeVotes: obj.negativevotes,
+        Creation: obj.lastchangetime
+      });
+    }
+    return db.StationHistory.bulkCreate(all);
+  });
+}
+
+function doIncrementalSync(){
+  console.log('doIncrementalSync()');
+  incrementalSync(SYNC_INTERVAL * 2).then(()=>{
+    console.log('Incremental sync done');
+  }).catch((err)=>{
+    console.log(err);
+  }).then(()=>{
+    setTimeout(doIncrementalSync, SYNC_INTERVAL_MSEC);
+  });
+}
+
+retryPromise(10, 3000, function () {
+  console.log('Connecting to db..');
+  return db.sequelize.authenticate();
+})
+  .then(() => {
+    console.log('Recreate db tables..');
+    return db.sequelize.sync({
+      force: true
+    });
   })
   .then(()=>{
-    console.log('OK');
-    return db.Station.findOne();
+    return initialSync();
   })
-  .then((s) => {
-    console.log('Found station:' + JSON.stringify(s, null, ' '));
-    return db.sequelize.close();
+  .then(()=>{
+  //   console.log('Selecting first station from DB..');
+  //   return db.Station.findOne();
+  // })
+  // .then((s) => {
+  //   console.log('Found station:' + JSON.stringify(s, null, ' '));
+    setTimeout(doIncrementalSync, SYNC_INTERVAL_MSEC);
+    // return db.sequelize.close();
   })
   .catch(err => {
     console.error('Unable to connect to the database:', err);
